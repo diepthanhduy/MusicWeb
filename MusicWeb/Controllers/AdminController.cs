e4d0501b29506d8c14dda09d780199317d11f323
Duy <diepthanhduy377@gmail.com>
2021-11-05 15:43:28 +0700

    Add project files.

---
+++ MusicWeb/Controllers/AdminController.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicWeb.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MusicWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbAdminDataContext db = new dbAdminDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            //Creact login view
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            //Gan gia tri
+            var email = collection["email"];
+            var matkhau = collection["password"];
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi1"] = "Phải nhập email";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập matkhau";
+            }
+            else
+            {
+                Admin ad = db.Admins.SingleOrDefault(n => n.Email == email && n.MatKhau == matkhau);
+                if (ad != null)
+                {
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Email hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DoiMK()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoiMK(FormCollection collection)
+        {
+            var matkhaucu = collection["MatKhauCu"];
+            var matkhaumoi = collection["MatKhauMoi"];
+            var nhaplaiMK = collection["NhaplaiMK"];
+            if (String.IsNullOrEmpty(matkhaucu))
+            {
+                ViewData["Loi1"] = "Hãy nhập mật khẩu cũ";
+            }
+            else if (String.IsNullOrEmpty(matkhaumoi))
+            {
+                ViewData["Loi2"] = "Hãy nhập mật khẩu mới";
+            }
+            else if (String.IsNullOrEmpty(nhaplaiMK))
+            {
+                ViewData["Loi3"] = "Hãy nhập lại mật khẩu mới";
+            }
+            else
+            {
+                //gan gi tri cho doi tuong dc tao moi
+                Admin ad = (Admin)Session["Taikhoanadmin"];
+                Admin admin = db.Admins.SingleOrDefault(n => n.MatKhau == matkhaucu);
+                if (matkhaucu != ad.MatKhau)
+                {
+                    ViewBag.Thongbao = "Mật khẩu cũ không chính xác";
+                }
+                else
+                {
+
+                    admin.MatKhau = matkhaumoi;
+                    UpdateModel(admin);
+                    db.SubmitChanges();
+                    Session["Taikhoanadmin"] = null;
+                    return RedirectToAction("Login");
+                }
+            }
+            return this.DoiMK();
+
+
+        }
+        public ActionResult Nhac(int ?page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            //return View(db.Nhacs.ToList());
+            return View(db.Nhacs.ToList().OrderBy(n => n.MaBaiNhac).ToPagedList(pageNumber, pageSize));
+        }
+        //QL Bài Hát
+        //Them moi bai hat
+        [HttpGet]
+        public ActionResult Themmoibaihat()
+        {
+            ViewBag.MaNgheSi = new SelectList(db.NgheSis.ToList().OrderBy(n => n.TenNgheSi), "MaNgheSi", "TenNgheSi");
+            ViewBag.MaLoai = new SelectList(db.TheLoais.ToList().OrderBy(n => n.TenLoai), "MaLoai", "TenLoai");
+            ViewBag.MaSangTac = new SelectList(db.SangTacs.ToList().OrderBy(n => n.TenNguoiST), "MaSangTac", "TenNguoiST");
+            ViewBag.MaAlbum = new SelectList(db.Albums.ToList().OrderBy(n => n.TenAlbum), "MaAlbum", "TenAlbum");
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Themmoibaihat(Nhac nhac, HttpPostedFileBase fileUpload/*, HttpPostedFileBase fileupanh*/)
+        {
+            ViewBag.MaNgheSi = new SelectList(db.NgheSis.ToList().OrderBy(n => n.TenNgheSi), "MaNgheSi", "TenNgheSi");
+            ViewBag.MaLoai = new SelectList(db.TheLoais.ToList().OrderBy(n => n.TenLoai), "MaLoai", "TenLoai");
+            ViewBag.MaSangTac = new SelectList(db.SangTacs.ToList().OrderBy(n => n.TenNguoiST), "MaSangTac", "TenNguoiST");
+            ViewBag.MaAlbum = new SelectList(db.Albums.ToList().OrderBy(n => n.TenAlbum), "MaAlbum", "TenAlbum");
+
+            if (fileUpload == null /*|| fileupanh == null*/)
+            {
+                ViewBag.Thongbao = "Thieu File";
+                return View();
+            }
+            //Them csdl
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    var fileName = Path.GetFileName(fileUpload.FileName);
+                    var pathAnh = Path.Combine(Server.MapPath("~/img"), fileName);
+                    //var fileName = Path.GetFileName(fileUpload.FileName);
+                    //var pathNhac = Path.Combine(Server.MapPath("~/music"), fileName);
+                    if (System.IO.File.Exists(pathAnh))
+                    {
+                        ViewBag.Thongbao = "Da ton tai file nhac";
+                    }
+                    else
+                    {
+                        fileUpload.SaveAs(pathAnh);
+                        //fileUpload.SaveAs(pathNhac);
+                    }
+                    //nhac.FileAnh = fileName;
+                    nhac.FileNhac = fileName;
+                    //Luu vao csdl
+                    db.Nhacs.InsertOnSubmit(nhac);
+                    db.SubmitChanges();
+                }
+                return RedirectToAction("Nhac");
+            }
+        }
+        //QL thể loại nhạc
+        //DS the loại nhạc
+        public ActionResult TheLoaiNhac(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            //return View(db.Nhacs.ToList());
+            return View(db.TheLoais.ToList().OrderBy(n => n.MaLoai).ToPagedList(pageNumber, pageSize));
+        }
+        //Thêm thể loại nhạc
+        [HttpGet]
+
+        public ActionResult ThemLoaiNhac()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemLoaiNhac(TheLoai theloainhac)
+        {
+            db.TheLoais.InsertOnSubmit(theloainhac);
+            db.SubmitChanges();
+            return RedirectToAction("TheLoaiNhac");
+        }
+        //Xóa
+        [HttpGet]
+        public ActionResult XoaTheLoaiNhac(int id)
+        {
+            TheLoai loainhac = db.TheLoais.SingleOrDefault(n => n.MaLoai == id);
+            ViewBag.Maloai = loainhac.MaLoai;
+            if (loainhac == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(loainhac);
+        }
+        [HttpPost, ActionName("XoaTheLoaiNhac")]
+        public ActionResult XacNhanXoa(int id)
+        {
+            TheLoai loainhac = db.TheLoais.SingleOrDefault(n => n.MaLoai == id);
+            ViewBag.Maloai = loainhac.MaLoai;
+            if (loainhac == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.TheLoais.DeleteOnSubmit(loainhac);
+            db.SubmitChanges();
+            return RedirectToAction("TheLoaiNhac");
+        }
+        //QL Nghệ Sĩ
+        //DS Nghệ Sĩ
+        public ActionResult DSNgheSi(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            return View(db.NgheSis.ToList().OrderBy(n => n.MaNgheSi).ToPagedList(pageNumber, pageSize));
+        }
+        //Thêm nghệ sĩ
+        [HttpGet]
+
+        public ActionResult ThemNgheSi()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemNgheSi(NgheSi nghesi)
+        {
+            db.NgheSis.InsertOnSubmit(nghesi);
+            db.SubmitChanges();
+            return RedirectToAction("DSNgheSi");
+        }
+        //Xóa
+        [HttpGet]
+        public ActionResult XoaNgheSi(int id)
+        {
+            NgheSi nghesi = db.NgheSis.SingleOrDefault(n => n.MaNgheSi == id);
+            ViewBag.MaNgheSi = nghesi.MaNgheSi;
+            if (nghesi == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(nghesi);
+        }
+        [HttpPost, ActionName("XoaNgheSi")]
+        public ActionResult XacNhanXoaNgheSi(int id)
+        {
+            NgheSi nghesi = db.NgheSis.SingleOrDefault(n => n.MaNgheSi == id);
+            ViewBag.MaNgheSi = nghesi.MaNgheSi;
+            if (nghesi == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.NgheSis.DeleteOnSubmit(nghesi);
+            db.SubmitChanges();
+            return RedirectToAction("DSNgheSi");
+        }
+        //QL Sáng Tác
+        //DS Nghệ Sĩ Sáng Tác
+        public ActionResult DSSangTac(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            return View(db.SangTacs.ToList().OrderBy(n => n.MaSangTac).ToPagedList(pageNumber, pageSize));
+        }
+        //Thêm sáng tác
+        [HttpGet]
+
+        public ActionResult ThemSangTac()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemSangTac(SangTac sangtac)
+        {
+            db.SangTacs.InsertOnSubmit(sangtac);
+            db.SubmitChanges();
+            return RedirectToAction("DSSangTac");
+        }
+        //Xóa
+        [HttpGet]
+        public ActionResult XoaSangTac(int id)
+        {
+            SangTac sangtac = db.SangTacs.SingleOrDefault(n => n.MaSangTac == id);
+            ViewBag.MaSangTac = sangtac.MaSangTac;
+            if (sangtac == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sangtac);
+        }
+        [HttpPost, ActionName("XoaSangTac")]
+        public ActionResult XacNhanXoaSangTac(int id)
+        {
+            SangTac sangtac = db.SangTacs.SingleOrDefault(n => n.MaSangTac == id);
+            ViewBag.MaSangTac = sangtac.MaSangTac;
+            if (sangtac == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.SangTacs.DeleteOnSubmit(sangtac);
+            db.SubmitChanges();
+            return RedirectToAction("DSSangTac");
+        }
+        //QL Album
+        //DS Album
+        public ActionResult DSAlbum(int? page)
+        {
+            int pageNumber = (page ?? 1);
+            int pageSize = 7;
+            return View(db.Albums.ToList().OrderBy(n => n.MaAlbum).ToPagedList(pageNumber, pageSize));
+        }
+        //Thêm sáng tác
+        [HttpGet]
+
+        public ActionResult ThemAlbum()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ThemAlbum(Album album)
+        {
+            db.Albums.InsertOnSubmit(album);
+            db.SubmitChanges();
+            return RedirectToAction("DSAlbum");
+        }
+        //Xóa
+        [HttpGet]
+        public ActionResult XoaAlbum(int id)
+        {
+            Album album = db.Albums.SingleOrDefault(n => n.MaAlbum == id);
+            ViewBag.MaAlbum = album.MaAlbum;
+            if (album == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(album);
+        }
+        [HttpPost, ActionName("XoaAlbum")]
+        public ActionResult XacNhanXoaAlbum(int id)
+        {
+            Album album = db.Albums.SingleOrDefault(n => n.MaAlbum == id);
+            ViewBag.MaAlbum = album.MaAlbum;
+            if (album == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            db.Albums.DeleteOnSubmit(album);
+            db.SubmitChanges();
+            return RedirectToAction("DSAlbum");
+        }
+    }
+}
